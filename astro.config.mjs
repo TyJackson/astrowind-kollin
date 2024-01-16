4d058f1fbeb3db53107f3fa22758bd136b6d5c13
William Jackson <ty@Williams-MBP.localdomain>
2024-01-15 21:20:10 -0500

    kollin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,60 +1,49 @@
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-// import { defineConfig, squooshImageService } from 'astro/config';
-// import sitemap from '@astrojs/sitemap';
-// import tailwind from '@astrojs/tailwind';
-// import mdx from '@astrojs/mdx';
-// import partytown from '@astrojs/partytown';
-// import icon from 'astro-icon';
-// import tasks from './src/utils/tasks';
-// import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin } from './src/utils/frontmatter.mjs';
-// import { ANALYTICS, SITE } from './src/utils/config.ts';
-// import netlify from "@astrojs/netlify/functions";
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
-// const whenExternalScripts = (items = []) => ANALYTICS.vendors.googleAnalytics.id && ANALYTICS.vendors.googleAnalytics.partytown ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig, squooshImageService } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
+import icon from 'astro-icon';
+import tasks from './src/utils/tasks';
+import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin } from './src/utils/frontmatter.mjs';
+import { ANALYTICS, SITE } from './src/utils/config.ts';
+import netlify from "@astrojs/netlify/functions";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const whenExternalScripts = (items = []) => ANALYTICS.vendors.googleAnalytics.id && ANALYTICS.vendors.googleAnalytics.partytown ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
 
-// // https://astro.build/config
-// export default defineConfig({
-//   site: SITE.site,
-//   base: SITE.base,
-//   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
-//   output: 'server',
-//   integrations: [tailwind({
-//     applyBaseStyles: false
-//   }), sitemap(), mdx(), icon({
-//     include: {
-//       tabler: ['*'],
-//       'flat-color-icons': ['template', 'gallery', 'approval', 'document', 'advertising', 'currency-exchange', 'voice-presentation', 'business-contact', 'database']
-//     }
-//   }), ...whenExternalScripts(() => partytown({
-//     config: {
-//       forward: ['dataLayer.push']
-//     }
-//   })), tasks()],
-//   image: {
-//     service: squooshImageService()
-//   },
-//   markdown: {
-//     remarkPlugins: [readingTimeRemarkPlugin],
-//     rehypePlugins: [responsiveTablesRehypePlugin]
-//   },
-//   vite: {
-//     resolve: {
-//       alias: {
-//         '~': path.resolve(__dirname, './src')
-//       }
-//     }
-//   },
-//   adapter: netlify()
-// });
-
-
-import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-
+// https://astro.build/config
 export default defineConfig({
+  site: SITE.site,
+  base: SITE.base,
+  trailingSlash: SITE.trailingSlash ? 'always' : 'never',
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sitemap(), mdx(), icon({
+    include: {
+      tabler: ['*'],
+      'flat-color-icons': ['template', 'gallery', 'approval', 'document', 'advertising', 'currency-exchange', 'voice-presentation', 'business-contact', 'database']
+    }
+  }), ...whenExternalScripts(() => partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  })), tasks()],
+  image: {
+    service: squooshImageService()
+  },
+  markdown: {
+    remarkPlugins: [readingTimeRemarkPlugin],
+    rehypePlugins: [responsiveTablesRehypePlugin]
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src')
+      }
+    }
+  },
+  adapter: netlify()
 });
